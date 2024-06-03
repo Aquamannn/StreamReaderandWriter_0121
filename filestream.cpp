8de1db1ebec6c052b35266460dd2587981c07fb2
Lenovo <9.3razzinayyuri@gmail.com>
2024-06-03 09:48:55 +0700

    add int main

--- filestream.cpp
+++ filestream.cpp
@@ -1,4 +1,16 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
+int main() {
+	string baris;
+
+	ofstream outfile;
+	outfile.open("contohfile.txt");
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	
+
+}
 
